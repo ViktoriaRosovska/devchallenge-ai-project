eee4cbd0442b487f9b6a123392a4290e7935a2bc
Viktoria Rosovska <torimeister@gmail.com>
2024-06-27 18:00:14 +0300

    pull scripts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "@svgr/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  include: ["src/**/*.jsx", "node_modules/**/*.jsx", "src/**/*.js", "node_modules/**/*.js"],
+  plugins: [[react()], [svgr()]],
+  server: {
+    proxy: {
+      "/api": {
+        // target: "https://",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+    },
+  },
+  extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
+
+  base: "/devchallenge-ai-project",
+});
